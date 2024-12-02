b3e58fe71381be5dc5530f493a8906c5338bfca6
Veera Raita <fvera003@edu.xamk.fi>
2024-12-02 11:42:27 +0200

    locker & shop fixes, new rarity added, new skins added, new pricing

--- Marble Game/Assets/Scripts/Skin.cs
+++ Marble Game/Assets/Scripts/Skin.cs
@@ -5,27 +5,17 @@ using UnityEngine;
 [System.Serializable]
 public class Skin
 {
-    public enum SkinName
-    {
-        Beach,
-        Catten,
-        Classic,
-        Kitten,
-        Magic,
-        Relic,
-        Voronoi
-    }
-
     public enum Rarity
     {
         Common,
         Rare,
+        Epic,
         Misc
     }
 
     [field: SerializeField] public int id { get; private set; }
     [field: SerializeField] public int price { get; private set; }
-    [field: SerializeField] public SkinName skinName { get; private set; }
+    [field: SerializeField] public string skinName { get; private set; }
     [field: SerializeField] public Rarity rarity { get; set; }
     [field: SerializeField] public Sprite sprite { get; private set; }
     [field: SerializeField] public bool owned { get; set; }
